53dca3877c56255f5956665f6fafc8fff660ba44
rickychan0611 <ric0611@gmail.com>
2025-06-29 21:31:34 -0700

    change to 60mintues

--- functions/index.js
+++ functions/index.js
@@ -668,7 +668,7 @@ You'll always find one you like!`;
 
 //send ad to tg group
 exports.scheduledSendAd = onSchedule({
-  schedule: "every 1 hour",
+  schedule: "every 60 minutes",
   timeZone: "America/Vancouver"
 }, async (event) => {
   try {
